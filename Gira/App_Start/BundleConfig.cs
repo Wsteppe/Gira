944326a5d3cc45af67da72be3bf7855f4dc9a6f4
wsteppe@kpmg.com <wsteppe@kpmg.com>
2017-01-14 21:41:56 +0100

    - Fancied up layout - Made use of Jquery

--- Gira/App_Start/BundleConfig.cs
+++ Gira/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Gira
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customJquery")
+                .Include("~/Scripts/customJquery.js"));
         }
     }
 }
---
+++ Gira/Scripts/customJquery.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $("#copyrightSpan").fadeIn(3500);
+
+    //dirty code, but good enough for non-production code
+    $("#adminDropDown").click(function () {
+        $("#adminIssues").slideToggle();
+    });
+
+    $("#managedDropDown").click(function () {
+        $("#managedIssues").slideToggle();
+    });
+
+    $("#responsibleDropDown").click(function () {
+        $("#responsibleIssues").slideToggle();
+    });
+
+    $("#createdDropDown").click(function () {
+        $("#createdIssues").slideToggle();
+    });
+});
